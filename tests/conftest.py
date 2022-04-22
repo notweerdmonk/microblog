e49e2e1463a8c138fb8a534b80b8191ab6aedf79
notweerdmonk <wrdmnk@gmail.com>
2022-04-22 11:42:54 +0000

    Unit tests to test user and post addition into database.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 # define fixtures for testing
 
 import pytest
-from app import app
+from app import app, db
 from app.models import User, Post
 from werkzeug.security import generate_password_hash
 from datetime import datetime, timezone, timedelta
@@ -14,10 +14,29 @@ def new_user():
     return user
 
 @pytest.fixture(scope='function')
+def add_new_user(new_user):
+    '''
+    In case of error the database can contain the test user.
+    If so delete the user first.
+    '''
+    user_from_db = User.query.filter_by(username=new_user.username).first()
+    if user_from_db is not None:
+        db.session.delete(user_from_db)
+        db.session.commit()
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    user_from_db = User.query.filter_by(username=new_user.username).first()
+    yield user_from_db
+
+    db.session.delete(user_from_db)
+    db.session.commit()
+
+@pytest.fixture(scope='function')
 def new_post():
     timestamp = datetime(year=2022, month=4, day=19,
-            hour=15, minute=30, second=0, microsecond=0,
-            tzinfo=timezone(timedelta(hours=5, minutes=30)))
+            hour=15, minute=30, second=0, microsecond=0)
 
     user = User(username='Rahul', email='rahul@mail.com',
             password_hash=generate_password_hash('password'))
@@ -25,6 +44,35 @@ def new_post():
     post = Post(timestamp=timestamp, body='Flask is awesome.', author=user)
 
     return user, post;
+
+@pytest.fixture(scope='function')
+def add_new_post(new_post):
+    '''
+    In case of error the database can contain the test user and post.
+    If so delete the user and post first.
+    '''
+    user, post = new_post
+
+    user_from_db = User.query.filter_by(username=user.username).first()
+    if user_from_db is not None:
+        post_from_db = Post.query.filter_by(author=user_from_db).first()
+        if post_from_db is not None:
+            db.session.delete(post_from_db)
+        db.session.delete(user_from_db)
+    db.session.commit()
+
+    db.session.add(user)
+    db.session.add(post)
+    db.session.commit()
+
+    user_from_db = User.query.filter_by(username=user.username).first()
+    if user_from_db is not None:
+        post_from_db = Post.query.filter_by(author=user_from_db).first()
+    yield user_from_db, post_from_db
+
+    db.session.delete(post_from_db)
+    db.session.delete(user_from_db)
+    db.session.commit()
 
 @pytest.fixture(scope='module')
 def test_client():
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -1,6 +1,7 @@
 import conftest
 from werkzeug.security import check_password_hash
 from datetime import datetime, timezone, timedelta
+from app import db
 
 def test_new_user(new_user):
     """
@@ -18,6 +19,16 @@ def test_new_user(new_user):
     assert new_user.is_anonymous == False
     assert new_user.get_id() == str(new_user.id)
 
+def test_add_new_user(add_new_user):
+    '''
+    GIVEN an instance of User model
+    WHEN the instance is added to database
+    THEN check if database is updated correctly
+    '''
+    assert add_new_user.username == 'Rahul'
+    assert add_new_user.email == 'rahul@mail.com'
+    assert check_password_hash(add_new_user.password_hash, 'password')
+
 def test_new_post(new_post):
     """
     GIVEN a Post model
@@ -27,8 +38,23 @@ def test_new_post(new_post):
     (user, post) = new_post
 
     timestamp = datetime(year=2022, month=4, day=19,
-            hour=15, minute=30, second=0, microsecond=0,
-            tzinfo=timezone(timedelta(hours=5, minutes=30)))
+            hour=15, minute=30, second=0, microsecond=0)
+
+    assert post.timestamp == timestamp
+    assert post.body == 'Flask is awesome.'
+    assert post.user_id == user.id
+    assert post.author == user
+
+def test_add_new_post(add_new_post):
+    """
+    GIVEN a Post model
+    WHEN a post is created
+    THEN check timestamp, body, user_id, author fields are valid
+    """
+    (user, post) = add_new_post
+
+    timestamp = datetime(year=2022, month=4, day=19,
+            hour=15, minute=30, second=0, microsecond=0)
 
     assert post.timestamp == timestamp
     assert post.body == 'Flask is awesome.'
