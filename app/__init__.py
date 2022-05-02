2c20f11ac1ac7d3ed8f28d64b9c5cced51438398
notweerdmonk <wrdmnk@gmail.com>
2022-05-02 05:56:20 +0000

    Chapter 12: Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
@@ -15,6 +16,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+moment = Moment(app)
 
 from app import routes, models, errors
 
