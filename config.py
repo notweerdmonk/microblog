f9d758b61395cace752c19ba21efb4a9787ecbe0
notweerdmonk <wrdmnk@gmail.com>
2022-06-24 17:03:24 +0000

    Change postgresql URI.

--- config.py
+++ config.py
@@ -16,9 +16,9 @@ class Config(object):
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '')\
+            .replace('postgres://', 'postgresql://') or \
+            'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
