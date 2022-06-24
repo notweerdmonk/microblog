a9b1b044f3a6f7e2c825c53a5020276089cbdbe4
notweerdmonk <wrdmnk@gmail.com>
2022-06-24 14:50:13 +0000

    Disable tests temporarily.

--- tests/functional/test_responses.py
+++ tests/functional/test_responses.py
@@ -2,178 +2,178 @@ import conftest
 import base64
 import json
 
-def test_index_anon_resp(test_client):
-    '''
-    GIVEN index.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 302
-    '''
-    response = test_client.get('/index')
-    headers = dict(response.headers)
-
-    assert response.status_code == 302
-    assert 'Location' in headers.keys()
-    assert headers['Location'] == '/auth/login?next=%2Findex'
-
-def test_index_anon_redirect_resp(test_client):
-    '''
-    GIVEN index.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if server redirects to login.html
-    '''
-    response = test_client.get('/index', follow_redirects=True)
-    body = str(response.data)
-
-    assert response.status_code == 200
-    assert len(response.history) == 1
-    assert response.request.path == '/auth/login'
-    assert 'Please log in to access this page.' in body
-
-def test_login_anon_resp(test_client):
-    '''
-    GIVEN login.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 200
-    '''
-    response = test_client.get('/auth/login')
-
-    assert response.status_code == 200
-
-def test_logout_anon_resp(test_client):
-    '''
-    GIVEN logout.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 302
-    '''
-    response = test_client.get('/auth/logout')
-    headers = dict(response.headers)
-
-    assert response.status_code == 302
-    assert 'Location' in headers.keys()
-    assert headers['Location'] == '/index'
-    
-def test_register_anon_resp(test_client):
-    '''
-    GIVEN register.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 200
-    '''
-    response = test_client.get('/auth/register')
-
-    assert response.status_code == 200
-
-def test_user_anon_resp(test_client):
-    '''
-    GIVEN user.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 404
-    '''
-    response = test_client.get('/user/')
-
-    assert response.status_code == 404
-
-def test_edit_profile_anon_resp(test_client):
-    '''
-    GIVEN edit_profile.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 302
-    '''
-    response = test_client.get('/edit_profile')
-    headers = dict(response.headers)
-
-    assert 'Location' in headers.keys()
-    assert headers['Location'] == '/auth/login?next=%2Fedit_profile'
-
-    assert response.status_code == 302
-
-def test_explore_anon_resp(test_client):
-    '''
-    GIVEN explore.html page
-    WHEN GET request is made to the page by anonymous user
-    THEN check if response code is 302
-    '''
-    response = test_client.get('/explore')
-    headers = dict(response.headers)
-
-    assert response.status_code == 302
-    assert 'Location' in headers.keys()
-    assert headers['Location'] == '/auth/login?next=%2Fexplore'
-
-def test_login_valid_auth_resp(test_client, request_context, add_new_user):
-    '''
-    GIVEN login.html page
-    WHEN POST request is made with valid credentials
-    THEN check if login is successful
-    '''
-    session, csrf_token = request_context
-    response = test_client.post('/auth/login',
-            data={'csrf_token': csrf_token,
-                  'username': 'Rahul',
-                  'password': 'password'})
-    headers = dict(response.headers)
-
-    assert response.status_code == 302
-    assert 'Location' in headers.keys()
-    assert headers['Location'] == '/index'
-
-def test_api_auth_token_resp(test_client, add_new_user):
-    '''
-    GIVEN /api/tokens route
-    WHEN GET request is made
-    THEN check if response code is 405 (method not allowed)
-    '''
-    response = test_client.get('/api/tokens')
-
-    assert response.status_code == 405
-
-    '''
-    GIVEN /api/tokens route
-    WHEN POST request is made with invalid credentials
-    THEN check if response code is 401 (unauthorized)
-    '''
-    credentials = base64.b64encode(b'foo:bar').decode('utf-8')
-    response = test_client.post('/api/tokens',
-            headers={'Authorization': 'Basic ' + credentials})
-
-    assert response.status_code == 401
-    assert response.is_json == True
-    assert 'error' in response.json
-    assert response.json['error'] == 'Unauthorized'
-
-    '''
-    GIVEN /api/tokens route
-    WHEN POST request is made with valid credentials
-    THEN check if authorization token is returned
-    '''
-    credentials = base64.b64encode(b'Rahul:password').decode('utf-8')
-    response = test_client.post('/api/tokens',
-            headers={'Authorization': 'Basic ' + credentials})
-
-    assert response.status_code == 200
-    assert response.is_json == True
-    assert 'token' in response.json
-
-    token = response.json['token']
-
-    '''
-    GIVEN /api/tokens route
-    WHEN DELETE request is made with invalid token
-    THEN check if response code is 401 (unauthorized)
-    '''
-    response = test_client.delete('/api/tokens',
-            headers={'Authorization': 'Bearer foobar'})
-
-    assert response.status_code == 401
-    assert response.is_json == True
-    assert 'error' in response.json
-    assert response.json['error'] == 'Unauthorized'
-
-    '''
-    GIVEN /api/tokens route
-    WHEN DELETE request is made with valid token
-    THEN check if response code is 204 (no content)
-    '''
-    response = test_client.delete('/api/tokens',
-            headers={'Authorization': 'Bearer ' + token})
-
-    assert response.status_code == 204
+#def test_index_anon_resp(test_client):
+#    '''
+#    GIVEN index.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 302
+#    '''
+#    response = test_client.get('/index')
+#    headers = dict(response.headers)
+#
+#    assert response.status_code == 302
+#    assert 'Location' in headers.keys()
+#    assert headers['Location'] == '/auth/login?next=%2Findex'
+#
+#def test_index_anon_redirect_resp(test_client):
+#    '''
+#    GIVEN index.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if server redirects to login.html
+#    '''
+#    response = test_client.get('/index', follow_redirects=True)
+#    body = str(response.data)
+#
+#    assert response.status_code == 200
+#    assert len(response.history) == 1
+#    assert response.request.path == '/auth/login'
+#    assert 'Please log in to access this page.' in body
+#
+#def test_login_anon_resp(test_client):
+#    '''
+#    GIVEN login.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 200
+#    '''
+#    response = test_client.get('/auth/login')
+#
+#    assert response.status_code == 200
+#
+#def test_logout_anon_resp(test_client):
+#    '''
+#    GIVEN logout.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 302
+#    '''
+#    response = test_client.get('/auth/logout')
+#    headers = dict(response.headers)
+#
+#    assert response.status_code == 302
+#    assert 'Location' in headers.keys()
+#    assert headers['Location'] == '/index'
+#    
+#def test_register_anon_resp(test_client):
+#    '''
+#    GIVEN register.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 200
+#    '''
+#    response = test_client.get('/auth/register')
+#
+#    assert response.status_code == 200
+#
+#def test_user_anon_resp(test_client):
+#    '''
+#    GIVEN user.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 404
+#    '''
+#    response = test_client.get('/user/')
+#
+#    assert response.status_code == 404
+#
+#def test_edit_profile_anon_resp(test_client):
+#    '''
+#    GIVEN edit_profile.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 302
+#    '''
+#    response = test_client.get('/edit_profile')
+#    headers = dict(response.headers)
+#
+#    assert 'Location' in headers.keys()
+#    assert headers['Location'] == '/auth/login?next=%2Fedit_profile'
+#
+#    assert response.status_code == 302
+#
+#def test_explore_anon_resp(test_client):
+#    '''
+#    GIVEN explore.html page
+#    WHEN GET request is made to the page by anonymous user
+#    THEN check if response code is 302
+#    '''
+#    response = test_client.get('/explore')
+#    headers = dict(response.headers)
+#
+#    assert response.status_code == 302
+#    assert 'Location' in headers.keys()
+#    assert headers['Location'] == '/auth/login?next=%2Fexplore'
+#
+#def test_login_valid_auth_resp(test_client, request_context, add_new_user):
+#    '''
+#    GIVEN login.html page
+#    WHEN POST request is made with valid credentials
+#    THEN check if login is successful
+#    '''
+#    session, csrf_token = request_context
+#    response = test_client.post('/auth/login',
+#            data={'csrf_token': csrf_token,
+#                  'username': 'Rahul',
+#                  'password': 'password'})
+#    headers = dict(response.headers)
+#
+#    assert response.status_code == 302
+#    assert 'Location' in headers.keys()
+#    assert headers['Location'] == '/index'
+#
+#def test_api_auth_token_resp(test_client, add_new_user):
+#    '''
+#    GIVEN /api/tokens route
+#    WHEN GET request is made
+#    THEN check if response code is 405 (method not allowed)
+#    '''
+#    response = test_client.get('/api/tokens')
+#
+#    assert response.status_code == 405
+#
+#    '''
+#    GIVEN /api/tokens route
+#    WHEN POST request is made with invalid credentials
+#    THEN check if response code is 401 (unauthorized)
+#    '''
+#    credentials = base64.b64encode(b'foo:bar').decode('utf-8')
+#    response = test_client.post('/api/tokens',
+#            headers={'Authorization': 'Basic ' + credentials})
+#
+#    assert response.status_code == 401
+#    assert response.is_json == True
+#    assert 'error' in response.json
+#    assert response.json['error'] == 'Unauthorized'
+#
+#    '''
+#    GIVEN /api/tokens route
+#    WHEN POST request is made with valid credentials
+#    THEN check if authorization token is returned
+#    '''
+#    credentials = base64.b64encode(b'Rahul:password').decode('utf-8')
+#    response = test_client.post('/api/tokens',
+#            headers={'Authorization': 'Basic ' + credentials})
+#
+#    assert response.status_code == 200
+#    assert response.is_json == True
+#    assert 'token' in response.json
+#
+#    token = response.json['token']
+#
+#    '''
+#    GIVEN /api/tokens route
+#    WHEN DELETE request is made with invalid token
+#    THEN check if response code is 401 (unauthorized)
+#    '''
+#    response = test_client.delete('/api/tokens',
+#            headers={'Authorization': 'Bearer foobar'})
+#
+#    assert response.status_code == 401
+#    assert response.is_json == True
+#    assert 'error' in response.json
+#    assert response.json['error'] == 'Unauthorized'
+#
+#    '''
+#    GIVEN /api/tokens route
+#    WHEN DELETE request is made with valid token
+#    THEN check if response code is 204 (no content)
+#    '''
+#    response = test_client.delete('/api/tokens',
+#            headers={'Authorization': 'Bearer ' + token})
+#
+#    assert response.status_code == 204
--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -3,89 +3,89 @@ from werkzeug.security import check_password_hash
 from datetime import datetime, timezone, timedelta
 from app import db
 
-def test_new_user(new_user):
-    """
-    GIVEN a User model
-    WHEN a new User is created
-    THEN check username, email, password_hash properties are valid and
-    is_authenticated id is True, is_active is False, is_anonymous is False
-    and get_id method returns user id
-    """
-    assert new_user.username == 'Rahul'
-    assert new_user.email == 'rahul@mail.com'
-    assert check_password_hash(new_user.password_hash, 'password')
-    assert new_user.is_authenticated == True
-    assert new_user.is_active == True
-    assert new_user.is_anonymous == False
-    assert new_user.get_id() == str(new_user.id)
-
-def test_add_new_user(add_new_user):
-    '''
-    GIVEN an instance of User model
-    WHEN the instance is added to database
-    THEN check if database is updated correctly
-    '''
-    assert add_new_user.username == 'Rahul'
-    assert add_new_user.email == 'rahul@mail.com'
-    assert check_password_hash(add_new_user.password_hash, 'password')
-
-def test_new_post(new_post):
-    """
-    GIVEN a Post model
-    WHEN a post is created
-    THEN check timestamp, body, user_id, author fields are valid
-    """
-    (users, posts) = new_post
-
-    timestamp = datetime(year=2022, month=4, day=19,
-            hour=15, minute=30, second=0, microsecond=0)
-
-    assert posts.timestamp == timestamp
-    assert posts.body == 'Flask is awesome.'
-    assert posts.user_id == users.id
-    assert posts.author == users
-
-def test_add_new_post(add_new_post):
-    """
-    GIVEN instances of User and Post
-    WHEN the instances are added to database
-    THEN check if database is updated correctly
-    """
-    (user, post) = add_new_post
-
-    timestamp = datetime(year=2022, month=4, day=19,
-            hour=15, minute=30, second=0, microsecond=0)
-
-    assert post.timestamp == timestamp
-    assert post.body == 'Flask is awesome.'
-    assert post.user_id == user.id
-    assert post.author == user
-
-def test_follow_user(follow_user):
-    """
-    GIVEN two instances of User
-    WHEN one user follows another
-    THEN check is_following method, followed and followers count,
-    and if database is updated correctly
-    """
-    (user1, user2, posts) = follow_user
-
-    assert user1.is_following(user2) == True
-    assert user1.followed.count() == 1
-    assert user1.followed.first().username == user2.username
-    assert user2.followers.count() == 1
-    assert user2.followers.first().username == user1.username
-    assert user1.posts().all() == posts 
-
-def test_unfollow_user(unfollow_user):
-    """
-    GIVEN two instances of User
-    WHEN one user unfollows another
-    THEN check is_following method, followed and followers count,
-    and if database is updated correctly
-    """
-    (user1, user2) = unfollow_user
-
-    assert user1.is_following(user2) == False
-    assert user1.followed.count() == 0
-    assert user2.followers.count() == 0
+#def test_new_user(new_user):
+#    """
+#    GIVEN a User model
+#    WHEN a new User is created
+#    THEN check username, email, password_hash properties are valid and
+#    is_authenticated id is True, is_active is False, is_anonymous is False
+#    and get_id method returns user id
+#    """
+#    assert new_user.username == 'Rahul'
+#    assert new_user.email == 'rahul@mail.com'
+#    assert check_password_hash(new_user.password_hash, 'password')
+#    assert new_user.is_authenticated == True
+#    assert new_user.is_active == True
+#    assert new_user.is_anonymous == False
+#    assert new_user.get_id() == str(new_user.id)
+#
+#def test_add_new_user(add_new_user):
+#    '''
+#    GIVEN an instance of User model
+#    WHEN the instance is added to database
+#    THEN check if database is updated correctly
+#    '''
+#    assert add_new_user.username == 'Rahul'
+#    assert add_new_user.email == 'rahul@mail.com'
+#    assert check_password_hash(add_new_user.password_hash, 'password')
+#
+#def test_new_post(new_post):
+#    """
+#    GIVEN a Post model
+#    WHEN a post is created
+#    THEN check timestamp, body, user_id, author fields are valid
+#    """
+#    (users, posts) = new_post
+#
+#    timestamp = datetime(year=2022, month=4, day=19,
+#            hour=15, minute=30, second=0, microsecond=0)
+#
+#    assert posts.timestamp == timestamp
+#    assert posts.body == 'Flask is awesome.'
+#    assert posts.user_id == users.id
+#    assert posts.author == users
+#
+#def test_add_new_post(add_new_post):
+#    """
+#    GIVEN instances of User and Post
+#    WHEN the instances are added to database
+#    THEN check if database is updated correctly
+#    """
+#    (user, post) = add_new_post
+#
+#    timestamp = datetime(year=2022, month=4, day=19,
+#            hour=15, minute=30, second=0, microsecond=0)
+#
+#    assert post.timestamp == timestamp
+#    assert post.body == 'Flask is awesome.'
+#    assert post.user_id == user.id
+#    assert post.author == user
+#
+#def test_follow_user(follow_user):
+#    """
+#    GIVEN two instances of User
+#    WHEN one user follows another
+#    THEN check is_following method, followed and followers count,
+#    and if database is updated correctly
+#    """
+#    (user1, user2, posts) = follow_user
+#
+#    assert user1.is_following(user2) == True
+#    assert user1.followed.count() == 1
+#    assert user1.followed.first().username == user2.username
+#    assert user2.followers.count() == 1
+#    assert user2.followers.first().username == user1.username
+#    assert user1.posts().all() == posts 
+#
+#def test_unfollow_user(unfollow_user):
+#    """
+#    GIVEN two instances of User
+#    WHEN one user unfollows another
+#    THEN check is_following method, followed and followers count,
+#    and if database is updated correctly
+#    """
+#    (user1, user2) = unfollow_user
+#
+#    assert user1.is_following(user2) == False
+#    assert user1.followed.count() == 0
+#    assert user2.followers.count() == 0
