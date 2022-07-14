8ad7380fd7218f0da8953ad0d8bdaecbd1aac8e6
notweerdmonk <wrdmnk@gmail.com>
2022-07-14 06:31:26 +0000

    Update readme.

--- app/auth/egg.py
+++ app/auth/egg.py
@@ -2,3 +2,4 @@ def egg(username, password):
     if username == 'admin' and password == 'sthmsg607':
         print('user discovered the egg!')
         return 'https://music.youtube.com/watch?v=UHa4TurvIx8&t=367'
+    return None
